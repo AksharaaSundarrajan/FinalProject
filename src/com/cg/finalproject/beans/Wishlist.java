64016d740d29600cd567e1046958d874e0091f4c
AksharaaSundarrajan <41157474+AksharaaSundarrajan@users.noreply.github.com>
2018-08-13 17:57:29 +0530

    Update Wishlist.java

--- src/com/cg/finalproject/beans/Wishlist.java
+++ src/com/cg/finalproject/beans/Wishlist.java
@@ -1,21 +1,39 @@
 package com.cg.finalproject.beans;
 
-public class Wishlist {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+@Entity
+public class Wishlist {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int wishListId;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="phoneNumber")
 	private Customer customer;// one to one
-	private Product product;// one to many
+	@OneToMany(mappedBy="wishlist",cascade=CascadeType.ALL)
+	private List<Product> products = new ArrayList<Product>();// one to many
 	public Customer getCustomer() {
 		return customer;
 	}
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	public Product getProduct() {
-		return product;
+	public List<Product> getProducts() {
+		return products;
 	}
-	public void setProduct(Product product) {
-		this.product = product;
+	public void setProducts(List<Product> products) {
+		this.products = products;
 	}
+	
 	
 	
 }
