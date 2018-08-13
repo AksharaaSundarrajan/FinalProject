25eeaa2e804b8f6e70301433038073945b0e8609
AksharaaSundarrajan <41157474+AksharaaSundarrajan@users.noreply.github.com>
2018-08-13 17:38:18 +0530

    Update Cart.java

--- src/com/cg/finalproject/beans/Cart.java
+++ src/com/cg/finalproject/beans/Cart.java
@@ -1,22 +1,52 @@
 package com.cg.finalproject.beans;
 
-public class Cart {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Cart {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int cartId;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="phoneNumber")
 	private Customer customer;//one to one
-	private Product product;//one to many
+	@OneToMany(mappedBy="cart",cascade=CascadeType.ALL)
+	private List<Product> products = new ArrayList<Product>();// one to many
 	private int quantityRequired;//max 5
 	private double totalAmount;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="couponId")
+	private Coupon coupon;// one to one
+	
+	
 	public Customer getCustomer() {
 		return customer;
 	}
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	public Product getProduct() {
-		return product;
+	
+	public List<Product> getProducts() {
+		return products;
 	}
-	public void setProduct(Product product) {
-		this.product = product;
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	public Coupon getCoupon() {
+		return coupon;
+	}
+	public void setCoupon(Coupon coupon) {
+		this.coupon = coupon;
 	}
 	public int getQuantityRequired() {
 		return quantityRequired;
