411ce2256511104ad49ee52d0c4f0ae6e8aa778a
AksharaaSundarrajan <41157474+AksharaaSundarrajan@users.noreply.github.com>
2018-08-13 17:42:40 +0530

    Update Customer.java

--- src/com/cg/finalproject/beans/Customer.java
+++ src/com/cg/finalproject/beans/Customer.java
@@ -1,20 +1,37 @@
 package com.cg.finalproject.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Customer {
 @Id
 private String phoneNumber;
-private Address address;
+@OneToOne(cascade=CascadeType.ALL)
+@JoinColumn(name="addressId")
+private Address address;	
 private String email;
 private String password;
 private String cardNumber;
 private String securityQuestion;
 private String securityAnswer;
+@OneToMany(mappedBy="customer",cascade=CascadeType.ALL)
+private List<Review> reviews = new ArrayList<Review>();// one to many
 
 
+public Address getAddress() {
+	return address;
+}
+public void setAddress(Address address) {
+	this.address = address;
+}
 public String getSecurityQuestion() {
 	return securityQuestion;
 }
@@ -26,12 +43,6 @@ public String getSecurityAnswer() {
 }
 public void setSecurityAnswer(String securityAnswer) {
 	this.securityAnswer = securityAnswer;
-}
-public Address getAddress() {
-	return address;
-}
-public void setAddress(Address address) {
-	this.address = address;
 }
 public String getPhoneNumber() {
 	return phoneNumber;
