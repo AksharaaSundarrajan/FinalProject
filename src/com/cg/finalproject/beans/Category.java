c2b16ac8fd3ea21b6be4e419d3f4549e2f39245e
AksharaaSundarrajan <41157474+AksharaaSundarrajan@users.noreply.github.com>
2018-08-13 17:40:38 +0530

    Update Category.java

--- src/com/cg/finalproject/beans/Category.java
+++ src/com/cg/finalproject/beans/Category.java
@@ -1,27 +1,42 @@
 package com.cg.finalproject.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Category {
 @Id
+@GeneratedValue(strategy=GenerationType.AUTO)
 private int categoryId;
-private Product product;
+@OneToMany(mappedBy="category",cascade=CascadeType.ALL)
+private List<Product> products = new ArrayList<Product>();// one to many
 private String categoryName;//Clothes, books, gadgets
 private String type;		// shirts, pants, dresses in clothing
+@ManyToOne
+@JoinColumn(name="inventoryId")
+private Inventory inventory;
 
-public int getCategoryId() {
-	return categoryId;
+
+public List<Product> getProducts() {
+	return products;
 }
-public void setCategoryId(int categoryId) {
-	this.categoryId = categoryId;
+public void setProducts(List<Product> products) {
+	this.products = products;
 }
-public Product getProduct() {
-	return product;
+public Inventory getInventory() {
+	return inventory;
 }
-public void setProduct(Product product) {
-	this.product = product;
+public void setInventory(Inventory inventory) {
+	this.inventory = inventory;
 }
 public String getCategoryName() {
 	return categoryName;
